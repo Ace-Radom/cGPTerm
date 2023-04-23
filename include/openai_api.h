2a0bf4a4252b3d7110ff155174b3bca3ffb20ab5
Ace_Radom <sichenradomlyu@gmail.com>
2023-04-23 17:36:37 +0200

    add openai_api subproject; upload cvector v1.0.5

---
+++ include/openai_api.h
@@ -0,0 +1,14 @@
+#ifndef _OPENAI_API_H_
+#define _OPENAI_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include<curl/curl.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/openai_api/openai_api.c
@@ -0,0 +1,1 @@
+#include"openai_api.h"
