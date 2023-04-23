02c633763bc1eb12c7097549391e495f717e47f3
Ace_Radom <sichenradomlyu@gmail.com>
2023-04-23 20:52:22 +0200

    update utils: add config vars

--- include/utils.h
+++ include/utils.h
@@ -5,10 +5,22 @@
 extern "C" {
 #endif
 
+#include<stdbool.h>
+
 #include"ezylog.h"
 #include"openai_api.h"
+#include"iniparser.h"
+
+typedef dictionary ini_t;
 
 extern ezylog_logger_t* logger;
+extern ini_t* config;
+
+extern char* OPENAI_API_KEY;
+extern double OPENAI_API_TIMEOUT;
+extern bool AUTO_GENERATE_TITLE;
+extern char* CHAT_SAVE_PERFIX;
+extern ezylog_priority_t LOG_LEVEL;
 
 #ifdef __cplusplus
 }
--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,10 @@
 #include"utils.h"
 
 ezylog_logger_t* logger = NULL;
+ini_t* config = NULL;
+
+char* OPENAI_API_KEY = NULL;
+double OPENAI_API_TIMEOUT = 30.0;
+bool AUTO_GENERATE_TITLE = true;
+char* CHAT_SAVE_PERFIX = NULL;
+ezylog_priority_t LOG_LEVEL = EZYLOG_PRIORITY_INFO;
