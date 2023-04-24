ca10ce7870c83172785f814c13bb2237d030c7b5
Ace_Radom <sichenradomlyu@gmail.com>
2023-04-24 19:05:14 +0200

    change all function args to const

--- include/rwcfg.h
+++ include/rwcfg.h
@@ -9,11 +9,11 @@ extern "C" {
 
 #define CFG_DEFAULT_PATH "./config.ini"
 
-int setcfg_OPENAI_API_KEY( char* __api_key );
-int setcfg_OPENAI_API_TIMEOUT( double __timeout );
+int setcfg_OPENAI_API_KEY( const char* __api_key );
+int setcfg_OPENAI_API_TIMEOUT( const double __timeout );
 int setcfg_AUTO_GENERATE_TITLE();
-int setcfg_CHAT_SAVE_PERFIX( char* __perfix );
-int setcfg_LOG_LEVEL( char* __level );
+int setcfg_CHAT_SAVE_PERFIX( const char* __perfix );
+int setcfg_LOG_LEVEL( const char* __level );
 
 #ifdef __cplusplus
 }
--- src/rwcfg/rwcfg.c
+++ src/rwcfg/rwcfg.c
@@ -1,10 +1,10 @@
 #include"rwcfg.h"
 
-int setcfg_OPENAI_API_KEY( char* __api_key ){
+int setcfg_OPENAI_API_KEY( const char* __api_key ){
     return config_setstr( "DEFAULT:OPENAI_API_KEY" , __api_key );
 }
 
-int setcfg_OPENAI_API_TIMEOUT( double __timeout ){
+int setcfg_OPENAI_API_TIMEOUT( const double __timeout ){
     return config_setdouble( "DEFAULT:OPENAI_API_TIMEOUT" , __timeout );
 }
 
@@ -15,10 +15,10 @@ int setcfg_AUTO_GENERATE_TITLE(){
         return config_setstr( "DEFAULT:AUTO_GENERATE_TITLE" , "True" );
 }
 
-int setcfg_CHAT_SAVE_PERFIX( char* __perfix ){
+int setcfg_CHAT_SAVE_PERFIX( const char* __perfix ){
     return config_setstr( "DEFAULT:CHAT_SAVE_PERFIX" , __perfix );
 }
 
-int setcfg_LOG_LEVEL( char* __level ){
+int setcfg_LOG_LEVEL( const char* __level ){
     return config_setstr( "DEFAULT:LOG_LEVEL" , parse_priority_to_str( parse_str_to_priority( __level ) ) );
 }
