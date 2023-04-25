761a1eb549cde903a6497a9b2f020281b914c516
Ace_Radom <sichenradomlyu@gmail.com>
2023-04-25 16:08:46 +0200

    add openai_t; finish openai service init and free

--- include/openai_api.h
+++ include/openai_api.h
@@ -1,11 +1,43 @@
 #ifndef _OPENAI_API_H_
-#define _OPENAI_H_
+#define _OPENAI_API_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdbool.h>
+
 #include<curl/curl.h>
+#include<pthread.h>
+#include<jansson.h>
+
+#include"ezylog.h"
+#include"utils.h"
+
+typedef struct {
+    char* endpoint;
+    struct curl_slist* headers;
+    json_t* messages;
+    char* model;
+    int tokens_limit;
+    long long total_tokens_spent;
+    int current_tokens;
+    char* title;
+
+    double credit_total_granted;
+    double credit_total_used;
+    double credit_used_this_month;
+} openai_t;
+
+extern openai_t* openai;
+
+void openai_init();
+
+void openai_free();
 
 #ifdef __cplusplus
 }
--- src/openai_api/openai_api.c
+++ src/openai_api/openai_api.c
@@ -1,1 +1,47 @@
 #include"openai_api.h"
+
+openai_t* openai = NULL;
+
+void openai_init(){
+    openai = ( openai_t* ) malloc( sizeof( openai_t ) );
+    openai -> endpoint = "https://api.openai.com/v1/chat/completions";
+    openai -> headers = NULL;
+    openai -> messages = json_array();
+    openai -> model = ( char* ) malloc( 64 );
+    strcpy( openai -> model , "gpt-3.5-turbo" );
+    openai -> tokens_limit = 4096;
+    openai -> total_tokens_spent = 0;
+    openai -> current_tokens = 0;
+    openai -> title = NULL;
+    openai -> credit_total_granted = 0.0;
+    openai -> credit_total_used = 0.0;
+    openai -> credit_used_this_month = 0.0;
+    // basic init
+
+    char* authorization_bearer_header_str = ( char* ) malloc( 128 );
+    sprintf( authorization_bearer_header_str , "%s%s" , "Authorization: Bearer " , OPENAI_API_KEY );
+    openai -> headers = curl_slist_append( openai -> headers , "Content-Type: application/json" );
+    openai -> headers = curl_slist_append( openai -> headers , authorization_bearer_header_str );
+    // headers init
+
+    json_t* prompt = json_object();
+    json_object_set_new( prompt , "role" , json_string( "system" ) );
+    json_object_set_new( prompt , "content" , json_string( "You are a helpful assistant. Knowledge cutoff: 2021-09" ) );
+    json_array_append_new( openai -> messages , prompt );
+    // prompt init
+
+    curl_global_init( CURL_GLOBAL_ALL );
+    // curl init
+
+    return;
+}
+
+void openai_free(){
+    curl_slist_free_all( openai -> headers );
+    json_decref( openai -> messages );
+    free( openai -> model );
+    if ( openai -> title )
+        free( openai -> title );
+    free( openai );
+    return;
+}
