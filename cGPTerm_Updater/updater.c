f5c41ec5400f8baa6e6efa3eb20ef2500d45bfee
Ace_Radom <sichenradomlyu@gmail.com>
2023-05-16 21:44:27 +0200

    finish updater package download function

--- cGPTerm_Updater/updater.c
+++ cGPTerm_Updater/updater.c
@@ -11,6 +11,7 @@
 bool package_connected = false;
 char* remote_version = NULL;
 char* local_version = NULL;
+long response_code = 0;
 
 typedef struct {
     char* ptr;
@@ -21,7 +22,7 @@ size_t curl_write_callback_function( char* ptr , size_t size , size_t nmemb , vo
 
 size_t curl_download_callback_function( void* ptr , size_t size , size_t nmemb , FILE* stream );
  
-int curl_progress_function( char* progress_data , double dltotal , double dlnow , double ultotal , double ulnow );
+int curl_progress_callback_function( char* progress_data , double dltotal , double dlnow , double ultotal , double ulnow );
 
 bool is_local_latest();
  
@@ -54,11 +55,12 @@ int main( int argc, char **argv )
     CURL* curl;
     CURLcode res;
     curl_data_t response_data = { NULL , 0 };
+
     curl = curl_easy_init();
     if ( !curl )
     {
         fprintf( stderr , "CURL init failed when getting remote version, exit\n" );
-        return 1;
+        return 128;
     }
 
     curl_easy_setopt( curl , CURLOPT_URL , "https://api.github.com/repos/Ace-Radom/cGPTerm/releases" );
@@ -73,10 +75,9 @@ int main( int argc, char **argv )
     if ( res != CURLE_OK )
     {
         fprintf( stderr , "Get remote version failed.\n" );
-        return 1;
+        return 128;
     }
 
-    long response_code;
     curl_easy_getinfo( curl , CURLINFO_RESPONSE_CODE , &response_code );
 
     json_t* root;
@@ -89,16 +90,16 @@ int main( int argc, char **argv )
         FILE* ferr = fopen( "./github_res_dump.json" , "w" );
         fputs( response_data.ptr , ferr );
         fclose( ferr );
-        return 1;
+        return 128;
     } // dump response, exit
 
     if ( response_code != 200 )
     {
-        fprintf( stderr , "Getting remote version failed: GitHub API responsed %d" , response_code );
+        fprintf( stderr , "Getting remote version failed: GitHub API responsed %ld" , response_code );
         FILE* ferr = fopen( "./github_res_dump.json" , "w" );
         fputs( response_data.ptr , ferr );
         fclose( ferr );
-        return 1;
+        return 128;
     } // response not 200 OK
 
     json_t* remote_version_jsonobj = json_array_get( root , 0 );
@@ -107,6 +108,7 @@ int main( int argc, char **argv )
     strcpy( remote_version , json_string_value( remote_version_jsonobj ) );
     // copy remote version
     json_decref( root );
+    curl_easy_cleanup( curl );
 
     printf( "Latest remote version: %s\n" , remote_version );
 
@@ -115,7 +117,8 @@ int main( int argc, char **argv )
     if ( cgpterm_version_pipe == NULL )
     {
         fprintf( stderr , "Cannot get cGPTerm local version, exit\n" );
-        return 1;
+        free( remote_version );
+        return 128;
     }
     local_version = ( char* ) malloc( 16 );
     fgets( local_version , 16 , cgpterm_version_pipe );
@@ -124,31 +127,94 @@ int main( int argc, char **argv )
 
     printf( "Local version: %s\n" , local_version );
 
-    // char *url = "https://github.com/PowerShell/PowerShell/releases/download/v7.3.4/PowerShell-7.3.4-win.msixbundle";
-    // char *progress_data = "* ";
+    if ( is_local_latest() )
+    {
+        printf( "cGPTerm on this device is already the latest cGPTerm release\nNothing to do, exit\n" );
+        free( remote_version );
+        free( local_version );
+        return 1;
+    } // local is the latest
 
+    printf( "Update available, do you want to continue? [Y/n] " );
+    char confirm;
+    scanf( " %c" , &confirm );
 
-    // FILE *outfile;
-    // curl = curl_easy_init();
-    // if(curl)
-    // {
-    //     outfile = fopen("tar", "wb");
- 
-    //     curl_easy_setopt(curl, CURLOPT_URL, url);
-    //     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION , 1L );
-    //     curl_easy_setopt(curl, CURLOPT_WRITEDATA, outfile);
-    //     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_download_callback_function );
-    //     curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L );
-    //     curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, curl_progress_function );
-    //     curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, progress_data);
-    //     package_connected = false;
-    //     res = curl_easy_perform(curl);
- 
-    //     fclose(outfile);
-    //     /* always cleanup */
-    //     curl_easy_cleanup(curl);
-    // }
-    // curl_global_cleanup();
+    if ( confirm != 'Y' )
+    {
+        printf( "Abort\n" );
+        free( remote_version );
+        free( local_version );
+        return 1;
+    } // abort
+
+    printf( "Start update cGPTerm to %s\n" , remote_version );
+
+    char* download_url = ( char* ) malloc( 512 );
+    char* download_tar_name = ( char* ) malloc( 64 );
+    sprintf( download_url , "https://github.com/Ace-Radom/cGPTerm/releases/download/%s/cGPTerm_%s.tar.gz" , remote_version , remote_version );
+    sprintf( download_tar_name , "%s/cGPTerm_%s.tar.gz" , install_path , remote_version );
+
+    FILE* tar = NULL;
+    tar = fopen( download_tar_name , "wb" );
+    if ( tar == NULL )
+    {
+        perror( "Create package file failed" );
+        return 128;
+    }
+    curl = curl_easy_init();
+    if ( !curl )
+    {
+        fprintf( stderr , "CURL init failed when downloading latest release package, exit\n" );
+        free( remote_version );
+        free( local_version );
+        free( download_url );
+        free( download_tar_name );
+        return 128;
+    }
+
+    curl_easy_setopt( curl , CURLOPT_URL , download_url );
+    curl_easy_setopt( curl , CURLOPT_FOLLOWLOCATION , 1L );
+    curl_easy_setopt( curl , CURLOPT_WRITEDATA , tar );
+    curl_easy_setopt( curl , CURLOPT_WRITEFUNCTION , curl_download_callback_function );
+    curl_easy_setopt( curl , CURLOPT_NOPROGRESS , 0L );
+    curl_easy_setopt( curl , CURLOPT_PROGRESSFUNCTION , curl_progress_callback_function );
+    curl_easy_setopt( curl , CURLOPT_PROGRESSDATA , download_tar_name );
+    package_connected = false;
+    // make curl request
+
+    res = curl_easy_perform( curl );
+    printf( "\n" );
+    if ( res != CURLE_OK )
+    {
+        fprintf( stderr , "Download latest release package failed.\n" );
+        fclose( tar );
+        remove( download_tar_name );
+        free( remote_version );
+        free( local_version );
+        free( download_url );
+        free( download_tar_name );
+        return 128;
+    }
+
+    curl_easy_getinfo( curl , CURLINFO_RESPONSE_CODE , &response_code );
+    if ( response_code != 200 )
+    {
+        fprintf( stderr , "Getting remote version failed: GitHub API responsed %ld" , response_code );
+        fclose( tar );
+        remove( download_tar_name );
+        free( remote_version );
+        free( local_version );
+        free( download_url );
+        free( download_tar_name );
+        return 128;
+    }
+
+    printf( "Package download complete\n" );
+
+    fclose( tar );
+    curl_easy_cleanup( curl );
+
+    curl_global_cleanup();
     return 0;
 }
 
@@ -176,7 +242,7 @@ size_t curl_download_callback_function( void* ptr , size_t size , size_t nmemb ,
     return fwrite( ptr , size , nmemb , stream );
 } 
 
-int curl_progress_function( char* progress_data , double dltotal , double dlnow , double ultotal , double ulnow ){
+int curl_progress_callback_function( char* progress_data , double dltotal , double dlnow , double ultotal , double ulnow ){
     if ( dltotal == 0 || dlnow == 0 )
         printf( "\rConnecting..." );
     else
@@ -184,7 +250,7 @@ int curl_progress_function( char* progress_data , double dltotal , double dlnow 
         if ( !package_connected )
         {
             package_connected = true;
-            printf( "\r\033[2K\rConnected, downloading PowerShell-7.3.4-win.msixbundle...\n" );
+            printf( "\r\033[2K\rConnected, downloading package to %s...\n" , progress_data );
         }
         printf( "\rProgress: %.4lf %%" , dlnow * 100.0 / dltotal );
     }
@@ -193,10 +259,6 @@ int curl_progress_function( char* progress_data , double dltotal , double dlnow 
 }
 
 bool is_local_latest(){
-    if ( strcmp( remote_version , "Unknown" ) == 0 )
-        return true;
-    // remote version didn't get
-
     int local_major , local_minor , local_build;
     int remote_major , remote_minor , remote_build;
     sscanf( CGPTERM_VERSION , "v%d.%d.%d" , &local_major , &local_minor , &local_build );
