f9a0a5dd6eaf01e71b1aa6b34ee22a8112f2a1ff
Ace_Radom <sichenradomlyu@gmail.com>
2023-05-17 09:46:12 +0200

    feat: finish updater basic function

--- cGPTerm_Updater/updater.c
+++ cGPTerm_Updater/updater.c
@@ -40,7 +40,7 @@ int main( int argc, char **argv )
     else
     {
         fprintf( stderr , "Cannot find cGPTerm installed path, exit\n" );
-        return 1;
+        return 128;
     }
     // get install path
 
@@ -60,6 +60,7 @@ int main( int argc, char **argv )
     if ( !curl )
     {
         fprintf( stderr , "CURL init failed when getting remote version, exit\n" );
+        curl_global_cleanup();
         return 128;
     }
 
@@ -74,7 +75,9 @@ int main( int argc, char **argv )
     res = curl_easy_perform( curl );
     if ( res != CURLE_OK )
     {
-        fprintf( stderr , "Get remote version failed.\n" );
+        fprintf( stderr , "Get remote version failed: %s\n" , curl_easy_strerror( res ) );
+        curl_easy_cleanup( curl );
+        curl_global_cleanup();
         return 128;
     }
 
@@ -90,6 +93,8 @@ int main( int argc, char **argv )
         FILE* ferr = fopen( "./github_res_dump.json" , "w" );
         fputs( response_data.ptr , ferr );
         fclose( ferr );
+        curl_easy_cleanup( curl );
+        curl_global_cleanup();
         return 128;
     } // dump response, exit
 
@@ -99,6 +104,8 @@ int main( int argc, char **argv )
         FILE* ferr = fopen( "./github_res_dump.json" , "w" );
         fputs( response_data.ptr , ferr );
         fclose( ferr );
+        curl_easy_cleanup( curl );
+        curl_global_cleanup();
         return 128;
     } // response not 200 OK
 
@@ -113,16 +120,20 @@ int main( int argc, char **argv )
     printf( "Latest remote version: %s\n" , remote_version );
 
     FILE* cgpterm_version_pipe = NULL;
-    cgpterm_version_pipe = popen( "./cgpterm --version" , "r" );
+    char* cgpterm_version_cmd = ( char* ) malloc( PATH_MAX );
+    sprintf( cgpterm_version_cmd , "%s/cgpterm --version" , install_path );
+    cgpterm_version_pipe = popen( cgpterm_version_cmd , "r" );
     if ( cgpterm_version_pipe == NULL )
     {
         fprintf( stderr , "Cannot get cGPTerm local version, exit\n" );
         free( remote_version );
+        curl_global_cleanup();
         return 128;
     }
     local_version = ( char* ) malloc( 16 );
     fgets( local_version , 16 , cgpterm_version_pipe );
     pclose( cgpterm_version_pipe );
+    free( cgpterm_version_cmd );
     // get local version
 
     printf( "Local version: %s\n" , local_version );
@@ -132,6 +143,7 @@ int main( int argc, char **argv )
         printf( "cGPTerm on this device is already the latest cGPTerm release\nNothing to do, exit\n" );
         free( remote_version );
         free( local_version );
+        curl_global_cleanup();
         return 1;
     } // local is the latest
 
@@ -144,13 +156,14 @@ int main( int argc, char **argv )
         printf( "Abort\n" );
         free( remote_version );
         free( local_version );
+        curl_global_cleanup();
         return 1;
     } // abort
 
     printf( "Start update cGPTerm to %s\n" , remote_version );
 
     char* download_url = ( char* ) malloc( 512 );
-    char* download_tar_name = ( char* ) malloc( 64 );
+    char* download_tar_name = ( char* ) malloc( PATH_MAX );
     sprintf( download_url , "https://github.com/Ace-Radom/cGPTerm/releases/download/%s/cGPTerm_%s.tar.gz" , remote_version , remote_version );
     sprintf( download_tar_name , "%s/cGPTerm_%s.tar.gz" , install_path , remote_version );
 
@@ -159,6 +172,11 @@ int main( int argc, char **argv )
     if ( tar == NULL )
     {
         perror( "Create package file failed" );
+        free( remote_version );
+        free( local_version );
+        free( download_url );
+        free( download_tar_name );
+        curl_global_cleanup();
         return 128;
     }
     curl = curl_easy_init();
@@ -169,6 +187,7 @@ int main( int argc, char **argv )
         free( local_version );
         free( download_url );
         free( download_tar_name );
+        curl_global_cleanup();
         return 128;
     }
 
@@ -186,13 +205,15 @@ int main( int argc, char **argv )
     printf( "\n" );
     if ( res != CURLE_OK )
     {
-        fprintf( stderr , "Download latest release package failed.\n" );
+        fprintf( stderr , "Download latest release package failed: %s\n" , curl_easy_strerror( res ) );
         fclose( tar );
         remove( download_tar_name );
         free( remote_version );
         free( local_version );
         free( download_url );
         free( download_tar_name );
+        curl_easy_cleanup( curl );
+        curl_global_cleanup();
         return 128;
     }
 
@@ -206,6 +227,8 @@ int main( int argc, char **argv )
         free( local_version );
         free( download_url );
         free( download_tar_name );
+        curl_easy_cleanup( curl );
+        curl_global_cleanup();
         return 128;
     }
 
@@ -213,6 +236,63 @@ int main( int argc, char **argv )
 
     fclose( tar );
     curl_easy_cleanup( curl );
+
+    printf( "Collecting installed cGPTerm files...\n" );
+
+    FILE* dfl = NULL;
+    char* dfl_path = ( char* ) malloc( PATH_MAX );
+    sprintf( dfl_path , "%s/DFL" , install_path );
+    dfl = fopen( dfl_path , "r" );
+    if ( dfl == NULL )
+    {
+        perror( "Open download file list failed" );
+        remove( download_tar_name );
+        free( remote_version );
+        free( local_version );
+        free( download_url );
+        free( download_tar_name );
+        curl_global_cleanup();
+        return 128;
+    }
+    char* files = ( char* ) malloc( 64 );
+    while ( ( fgets( files , 64 , dfl ) ) != NULL )
+    {
+        char* this_file_path = ( char* ) malloc( PATH_MAX );
+        sprintf( this_file_path , "%s/%s" , install_path , files );
+        this_file_path[strlen(this_file_path)-1] = '\0';
+        // delete endline
+        printf( "Remove %s\n" , this_file_path );
+        remove( this_file_path );
+        free( this_file_path );
+    } // remove old files
+    free( files );
+    fclose( dfl );
+    free( dfl_path );
+
+    printf( "Install new cGPTerm release\nUnpacking package...\n" );
+
+    char* install_cmd = ( char* ) malloc( PATH_MAX + 64 );
+    sprintf( install_cmd , "tar -xzvf %s -C %s" , download_tar_name , install_path );
+    int sysrc = system( install_cmd );
+    free( install_cmd );
+    if ( sysrc != 0 )
+    {
+        fprintf( stderr , "Unpack package failed\n" );
+        remove( download_tar_name );
+        free( remote_version );
+        free( local_version );
+        free( download_url );
+        free( download_tar_name );
+        curl_global_cleanup();
+        return 128;
+    } // tar return not 0
+
+    printf( "Unpack success\ncGPTerm has been successfully updated to %s\n" , remote_version );
+    remove( download_tar_name );
+    free( remote_version );
+    free( local_version );
+    free( download_url );
+    free( download_tar_name );
 
     curl_global_cleanup();
     return 0;
